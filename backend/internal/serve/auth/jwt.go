4c23bbf430ca063ac243b608ec3fc6a09e4ce692
Alex Meuer <alex@alexmeuer.com>
2021-10-21 15:28:30 +0100

    cp

--- backend/internal/serve/auth/jwt.go
+++ backend/internal/serve/auth/jwt.go
@@ -1,12 +1,14 @@
 package auth
 
 import (
+	"errors"
+	"fmt"
 	"time"
 
 	"github.com/golang-jwt/jwt"
 )
 
-type JWTCreator struct {
+type JWTManager struct {
 	AccessSecret  []byte
 	RefreshSecret []byte
 	AccessTTL     time.Duration
@@ -14,23 +16,28 @@ type JWTCreator struct {
 	GenerateUUID  func() string
 }
 
-type TokenDetails struct {
-	AccessToken  string
-	RefreshToken string
-	AccessUUID   string
-	RefreshUUID  string
-	AtExpires    int64
-	RtExpires    int64
+type OutboundTokenDetails struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+	AccessUUID   string `json:"-"`
+	RefreshUUID  string `json:"-"`
+	AtExpires    int64  `json:"-"`
+	RtExpires    int64  `json:"-"`
 }
 
-func (c *JWTCreator) CreateToken(userID string, namespacedClaims map[string]jwt.MapClaims) (*TokenDetails, error) {
-	now := jwt.TimeFunc()
-	td := &TokenDetails{}
-	td.AtExpires = now.Add(c.AccessTTL).Unix()
-	td.AccessUUID = c.GenerateUUID()
+type InboundTokenDetails struct {
+	UUID   string
+	UserID string
+}
 
-	td.RtExpires = now.Add(c.RefreshTTL).Unix()
-	td.RefreshUUID = c.GenerateUUID()
+func (m *JWTManager) Create(userID string, namespacedClaims map[string]jwt.MapClaims) (*OutboundTokenDetails, error) {
+	now := jwt.TimeFunc()
+	td := &OutboundTokenDetails{}
+	td.AtExpires = now.Add(m.AccessTTL).Unix()
+	td.AccessUUID = m.GenerateUUID()
+
+	td.RtExpires = now.Add(m.RefreshTTL).Unix()
+	td.RefreshUUID = m.GenerateUUID()
 
 	var err error
 	// Creating Access Token
@@ -44,7 +51,8 @@ func (c *JWTCreator) CreateToken(userID string, namespacedClaims map[string]jwt.
 		atClaims[key] = value
 	}
 	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
-	td.AccessToken, err = at.SignedString(c.AccessSecret)
+	at.Header["kid"] = "a"
+	td.AccessToken, err = at.SignedString(m.AccessSecret)
 	if err != nil {
 		return nil, err
 	}
@@ -58,6 +66,67 @@ func (c *JWTCreator) CreateToken(userID string, namespacedClaims map[string]jwt.
 		atClaims[key] = value
 	}
 	rt := jwt.NewWithClaims(jwt.SigningMethodHS256, rtClaims)
-	td.RefreshToken, err = rt.SignedString(c.RefreshSecret)
+	rt.Header["kid"] = "r"
+	td.RefreshToken, err = rt.SignedString(m.RefreshSecret)
 	return td, err
+}
+
+func (m *JWTManager) Verify(tknStr string) (*jwt.Token, error) {
+	return jwt.Parse(tknStr, func(tkn *jwt.Token) (interface{}, error) {
+		//Make sure that the token method conform to "SigningMethodHMAC"
+		if _, ok := tkn.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", tkn.Header["alg"])
+		}
+		kID, ok := tkn.Header["kid"]
+		if !ok {
+			return nil, errors.New("no kid (key id) in token header")
+		}
+		switch kID {
+		case "a":
+			return m.AccessSecret, nil
+		case "r":
+			return m.RefreshSecret, nil
+		default:
+			return nil, fmt.Errorf("unexpected kid (key id) in token header: %v", kID)
+		}
+	})
+}
+
+// ExtractDetail attempts to extract the UUID and UserID from the given JWT.
+// If reqKID is not empty, then the key ID header of the JWT must match it or an
+// error will be returned.
+func ExtractDetail(tkn *jwt.Token, reqKID string) (*InboundTokenDetails, error) {
+	if !tkn.Valid {
+		return nil, errors.New("token is not valid")
+	}
+	keyID, ok := tkn.Header["kid"].(string)
+	if !ok {
+		return nil, fmt.Errorf("unable to read token key id from header: %v", tkn.Header)
+	}
+	if reqKID != "" && keyID != reqKID {
+		return nil, fmt.Errorf("unexpected key id '%s' but got '%s'", reqKID, keyID)
+	}
+	var UUIDClaimKey string
+	switch keyID {
+	case "a":
+		UUIDClaimKey = "access_uuid"
+	case "r":
+		UUIDClaimKey = "refesh_uuid"
+	default:
+		return nil, fmt.Errorf("unrecognized key id: %s", keyID)
+	}
+	claims, ok := tkn.Claims.(jwt.MapClaims)
+	if !ok {
+		return nil, fmt.Errorf("unable to read token claims as map claims: %v", tkn.Claims)
+	}
+	details := &InboundTokenDetails{}
+	details.UUID, ok = claims[UUIDClaimKey].(string)
+	if !ok {
+		return nil, fmt.Errorf("failed to find %s in token claims", UUIDClaimKey)
+	}
+	details.UserID, ok = claims["user_id"].(string)
+	if !ok {
+		return nil, errors.New("failed to find user_id in token claims")
+	}
+	return details, nil
 }
